a7e56878893d47435468e9324ac595e7515e4e5a
cudjoeab <cudjoeab@gmail.com>
2019-09-01 22:42:18 -0400

    added test to check any input

--- team.test.js
+++ team.test.js
@@ -1,5 +1,16 @@
 const teamObj = require('./team.js');
 
-// test('', () => {
-//
-// });
+test('when input is supplied, an object should be returned', () => {
+    const result = new teamObj('Optimus Prime', 10, 0); 
+
+
+    expect(result).toBeInstanceOf(Object);
+    expect(result.teamName).toEqual('Optimus Prime');
+    expect(result.level).toEqual(10);
+    expect(result.points).toEqual(0); 
+
+});
+
+
+
+
